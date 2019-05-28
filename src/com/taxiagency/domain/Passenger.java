9d37c90b01f3f6289d07f7d0545093ddc52af651
Иван <ivan.bury88@gmail.com>
2019-05-28 21:14:14 +0300

    Вроде получилость DriveRamDao

--- src/com/taxiagency/domain/Passenger.java
+++ src/com/taxiagency/domain/Passenger.java
@@ -5,6 +5,20 @@ public class Passenger implements Entity{
     private String name;
     private String mobilePhone;
 
+    public Passenger(String id, String name, String mobilePhone) {
+        this.id = id;
+        this.name = name;
+        this.mobilePhone = mobilePhone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
     @Override
     public String getId() {
         return null;
--- src/com/taxiagency/domain/Trip.java
+++ src/com/taxiagency/domain/Trip.java
@@ -5,8 +5,37 @@ public class Trip implements Entity {
     private String route;
     Driver driver;
     Car card;
-    //Pessenger pessenger;
+    Passenger pessenger;
     private int durarionMins;
+
+    public Trip(String id, String route, Driver driver, Car card, Passenger pessenger, int durarionMins) {
+        this.id = id;
+        this.route = route;
+        this.driver = driver;
+        this.card = card;
+        this.pessenger = pessenger;
+        this.durarionMins = durarionMins;
+    }
+
+    public String getRoute() {
+        return route;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public Car getCard() {
+        return card;
+    }
+
+    public Passenger getPessenger() {
+        return pessenger;
+    }
+
+    public int getDurarionMins() {
+        return durarionMins;
+    }
 
     @Override
     public String getId() {
